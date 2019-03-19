519973d4899df6207f948c3b0224a1ffde862900
tripdancer0916@gmail.com <ritalin202>
2019-03-19 21:41:30 +0900

    implement in pytorch

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class RecurrentNet(nn.Module):
         self.n_hid = n_hid
         self.n_out = n_out
         self.in_layer = nn.Linear(n_in, n_hid)
-        self.hid_layer = nn.RNNCell(n_hid, n_hid, nonlinearity='tanh')
+        self.hid_layer = nn.RNNCell(n_hid, n_hid, nonlinearity='relu')
         self.out_layer = nn.Linear(n_hid, n_out)
 
     def forward(self, input_signal):
@@ -25,7 +25,7 @@ class RecurrentNet(nn.Module):
         input_signal = input_signal.permute(1, 0, 2)
         # print(input_signal.shape)
         for t in range(length):
-            x = F.relu(self.in_layer(input_signal[t]))
+            x = self.in_layer(input_signal[t])
             hidden = self.hid_layer(x, hidden)
             output = self.out_layer(hidden)
             hidden_list[t] = hidden
