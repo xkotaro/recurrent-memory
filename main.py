a55f0fdd04f360a9e7f935c5f916220a21176d01
tripdancer0916@gmail.com <ritalin202>
2019-03-19 23:46:54 +0900

    implement in pytorch

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def train(model, device, train_loader, optimizer, epoch):
         _, output = model(data)
         # print(output.requires_grad)
 
-        loss = torch.nn.MSELoss()(output[:, -35:, :], target[:, -35:, :])
+        loss = torch.nn.MSELoss()(output[:, -25:, :], target[:, -25:, :])
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
