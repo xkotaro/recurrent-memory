bd30d4a4ea9e477f173cebcfa505b39581e06271
tripdancer0916@gmail.com <ritalin202>
2019-03-20 14:55:45 +0900

    introducing time scale

--- model.py
+++ model.py
@@ -23,9 +23,10 @@ class RecurrentNet(nn.Module):
         output_list = torch.zeros(length, num_batch, self.n_out, requires_grad=True).type_as(input_signal.data)
         input_signal = input_signal.permute(1, 0, 2)
         # print(input_signal.shape)
+        alpha = torch.Tensor([0.1])
         for t in range(length):
             x = self.in_layer(input_signal[t])
-            hidden = self.hid_layer(x, hidden)
+            hidden = (1-alpha) * x + alpha * self.hid_layer(x, hidden)
             output = self.out_layer(hidden)
             hidden_list[t] = hidden
             output_list[t] = output
