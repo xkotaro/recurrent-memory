195316a788fc305ca6f8441df3d98dbc09c343c8
tripdancer0916 <tripdancer0916@gmail.com>
2019-04-03 18:39:15 +0900

    prepare for another task

---
+++ make_lsm_signals.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+
+def lsm_signals(n_episodes=100, n_in=100, stim_dur=15,
+                sig1_stim_dur=20, resp_dur=10, each_episodes=10, kappa=5.0, spon_rate=0.08, n_stim=3):
+    phi = np.linspace(0, np.pi, n_in)
+    n_loc = 1
+    nneuron = n_in * n_loc
+    total_dur = n_stim * (stim_dur + resp_dur)
+    G = (1.0 / stim_dur) * np.random.choice([1.0], 1)
+    G = np.repeat(G, n_in, axis=0).T
+    G = np.tile(G, (stim_dur, 1))
+
+    # signal2
+    Stims = []
+    Stims_ = []
+    Ls = []
+    Rs = []
+    for episode in range(n_episodes):
+        episode_stim = []
+        for i in range(n_stim):
+            S = np.pi * np.random.rand(1)
+            S_ = S.copy()
+            S = np.repeat(S, n_in, axis=0).T
+            S = np.tile(S, (stim_dur, 1))
+            Stims.append(S)
+            episode_stim.append(S_)
+
+            # Noisy responses
+            L = G * np.exp(kappa * (np.cos(
+                2.0 * (S - np.tile(phi, (stim_dur, n_loc)))) - 1.0))  # stim
+
+            Ls.append(L)
+            R = np.random.poisson(L)
+            Rs.append(R)
+        Stims_.append(episode_stim)
+        Lr = (spon_rate / resp_dur) * np.ones((resp_dur * n_stim, nneuron))  # resp
+        Rr = np.random.poisson(Lr)
+
+        Rs.append(Rr)
+
+    signal2 = np.concatenate(tuple(Rs), axis=0)
+
+    G1 = (3.0 / sig1_stim_dur) * np.random.choice([1.0], 1)
+    G1 = np.repeat(G1, n_in, axis=0).T
+    G1 = np.tile(G1, (sig1_stim_dur, 1))
+    # signal1 & target
+    a = np.random.poisson(0.8, n_episodes)
+    Rs1 = []
+    accum_signal = np.pi * np.random.rand(1)
+    target_list = []
+
+    for episode in range(n_episodes):
+        target_list.append(np.zeros(stim_dur * n_stim))
+        if a[episode] == 2 or episode % each_episodes == 0:
+            accum_signal = np.pi * np.random.rand(1)
+            S = np.repeat(accum_signal, n_in, axis=0).T
+            S = np.tile(S, (sig1_stim_dur, 1))
+
+            L = G1 * np.exp(kappa * (np.cos(
+                2.0 * (S - np.tile(phi, (sig1_stim_dur, n_loc)))) - 1.0))  # stim
+            R = np.random.poisson(L)
+            Rs1.append(R)
+        else:
+            Lr = (spon_rate / resp_dur) * np.ones((sig1_stim_dur, nneuron))  # resp
+            R = np.random.poisson(Lr)
+            Rs1.append(R)
+        L_spont = (spon_rate / resp_dur) * np.ones((total_dur - sig1_stim_dur, nneuron))  # resp
+        R = np.random.poisson(L_spont)
+        Rs1.append(R)
+
+        for i in range(n_stim):
+            target = np.repeat(Stims_[episode][i]+accum_signal, resp_dur, axis=0)
+            target_list.append(target)
+
+    signal1 = np.concatenate(tuple(Rs1), axis=0)
+
+    target = np.concatenate(tuple(target_list), axis=0)
+    target = np.expand_dims(target, 1)
+
+    signal = np.concatenate((signal1, signal2), axis=1)
+    return signal, target
