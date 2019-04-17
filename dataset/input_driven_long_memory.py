a7701d2688cd7339ca1cd0a94c7eb31cbb0f3c5b
tripdancer0916 <tripdancer0916@gmail.com>
2019-04-17 12:59:16 +0900

    新しいタスク

---
+++ dataset/input_driven_long_memory.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+def input_driven_long_signals(n_episodes=100, n_in=100, stim_dur=15,
+                              resp_dur=10, kappa=5.0, spon_rate=0.08, n_stim=3):
+    phi = np.linspace(0, np.pi, n_in)
+    n_loc = 1
+    nneuron = n_in * n_loc
+    G = (1.0 / stim_dur) * np.random.choice([1.0], 1)
+    G = np.repeat(G, n_in, axis=0).T
+    G = np.tile(G, (stim_dur, 1))
+
+    Stims = []
+    Stims_ = []
+    Ls = []
+    Rs = []
+    for episode in range(n_episodes):
+        episode_stim = []
+        for i in range(n_stim):
+            S = np.pi * np.random.rand(1)
+            S_ = S.copy()
+            S = np.repeat(S, n_in, axis=0).T
+            S = np.tile(S, (stim_dur, 1))
+            Stims.append(S)
+            episode_stim.append(S_)
+            L = G * np.exp(kappa * (np.cos(
+                2.0 * (S - np.tile(phi, (stim_dur, n_loc)))) - 1.0))
+            Ls.append(L)
+            R = np.random.poisson(L)
+            Rs.append(R)
+        Stims_.append(episode_stim)
+        Lr = (spon_rate / resp_dur) * np.ones((resp_dur * n_stim, nneuron))  # resp
+        Rr = np.random.poisson(Lr)
+
+        Rs.append(Rr)
+
+    signal = np.concatenate(tuple(Rs), axis=0)
+    target_list = []
+
+    for episode in range(n_episodes):
+        if episode >= 3 and 2.0 <= Stims_[episode-3][0] + Stims_[episode-3][1] <= 3.0:
+            # print(episode)
+            for i in range(2):
+                target = np.repeat(Stims_[episode-3][i], resp_dur, axis=0)
+                target_list.append(target)
+            target_list.append(np.zeros(n_stim*stim_dur-2*resp_dur))
+        else:
+            target_list.append(np.zeros(stim_dur * n_stim))
+
+        for i in range(n_stim):
+            target = np.repeat(Stims_[episode][i], resp_dur, axis=0)
+            target_list.append(target)
+
+    target = np.concatenate(tuple(target_list), axis=0)
+    target = np.expand_dims(target, 1)
+
+    return signal, target
---
+++ train_input_driven_long_memory.py
@@ -0,0 +1,126 @@
+from __future__ import print_function
+
+import argparse
+import os
+from datetime import datetime
+
+import numpy as np
+import pytz
+import torch
+import torch.optim as optim
+import torch.utils.data
+
+from dataset import input_driven_long_memory
+from model.model import RecurrentNetTimeFixed
+
+
+def train(model, device, optimizer, stim_dur, each_episodes, resp_dur, n_stim, epoch, batch_size, n_hid):
+    model.train()
+    signals = []
+    targets = []
+    for i in range(batch_size):
+        signal, target = input_driven_long_memory.input_driven_long_signals(n_episodes=500, stim_dur=stim_dur,
+                                                                            resp_dur=resp_dur,
+                                                                            spon_rate=0.01)
+        signals.append(signal)
+        targets.append(target)
+
+    signals = np.array(signals)
+    targets = np.array(targets)
+
+    signals = torch.from_numpy(signals)
+    targets = torch.from_numpy(targets)
+
+    hidden = torch.zeros(batch_size, n_hid, requires_grad=True)
+    hidden = hidden.to(device)
+    one_learning_length = n_stim * (resp_dur + stim_dur)
+    for episodes in range(batch_size):
+        batched_signals = \
+            signals[:, episodes * one_learning_length * each_episodes:
+                       (episodes + 1) * one_learning_length * each_episodes, :]
+        batched_targets = \
+            targets[:, episodes * one_learning_length * each_episodes:
+                       (episodes + 1) * one_learning_length * each_episodes, :]
+
+        batched_signals = batched_signals.float()
+        batched_targets = batched_targets.float()
+        batched_signals.requires_grad = True
+        batched_signals, batched_targets = batched_signals.to(device), batched_targets.to(device)
+
+        optimizer.zero_grad()
+        hidden = hidden.detach()
+        _, output, hidden = model(batched_signals, hidden)
+
+        loss = torch.nn.MSELoss()(output,
+                                  batched_targets)
+        for i in range(each_episodes - 1):
+            loss += torch.nn.MSELoss()(output,
+                                       batched_targets)
+        loss.backward()
+        optimizer.step()
+        print("target: ", end=" ")
+        for i in range(n_stim, 0, -1):
+            print(batched_targets.cpu().data[0][-int(resp_dur * i)].numpy()[0], end=" ")
+        print('\n')
+        print("output: ", end=" ")
+        for i in range(n_stim, 0, -1):
+            print(output.cpu().data[0][-int(resp_dur * i)].numpy()[0], end=" ")
+        print("\n")
+        print('Train Epoch: {}, Episode: {}, Loss: {:.6f}'.format(
+            epoch, episodes, loss.item()))
+
+
+def main():
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print(device)
+
+    os.makedirs("~/models", exist_ok=True)
+
+    model = RecurrentNetTimeFixed(n_in=200, n_hid=args.network_size, n_out=1,
+                                  use_cuda=use_cuda).to(device)
+    print(model)
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
+
+    for epoch in range(1, args.epochs + 1):
+        train(model=model, device=device, optimizer=optimizer, stim_dur=args.stim_dur, each_episodes=args.each_episodes,
+              resp_dur=args.resp_dur, n_stim=args.n_stim, epoch=epoch, batch_size=args.batch_size,
+              n_hid=args.network_size)
+
+        if args.save_model and (epoch - 1) % args.savepoint == 0:
+            time_stamp = datetime.strftime(datetime.now(pytz.timezone('Japan')), '%m%d%H%M')
+            torch.save(
+                model.state_dict(),
+                "/root/models/{}_input_driven_slow_signals_fxtime_netsize_{}_stimdur_{}_nstim_{}_respdur_{}_epoch_{}.pth"
+                    .format(time_stamp, args.network_size, args.stim_dur, args.n_stim, args.resp_dur, epoch))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch RNN training')
+    parser.add_argument('--batch_size', type=int, default=50, metavar='N',
+                        help='input batch size for training (default: 50)')
+    parser.add_argument('--n_stim', type=int, default=3)
+    parser.add_argument('--each_episodes', type=int, default=5)
+    parser.add_argument('--stim_dur', type=int, default=15)
+    parser.add_argument('--resp_dur', type=int, default=10)
+    parser.add_argument('--t_constant', type=float, default=0.2)
+    parser.add_argument('--network_size', type=int, default=500)
+    parser.add_argument('--test_batch_size', type=int, default=50, metavar='N',
+                        help='input batch size for testing (default: 50)')
+    parser.add_argument('--savepoint', type=int, default=10)
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.0005, metavar='LR',
+                        help='learning rate (default: 0.0005)')
+    parser.add_argument('--no_cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log_interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save_model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    print(args)
+    main()
