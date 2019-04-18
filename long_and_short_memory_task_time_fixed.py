b3ab1b2a83446c7b6af3b1819d4df3f78e3f0a08
tripdancer0916 <tripdancer0916@gmail.com>
2019-04-18 11:50:22 +0900

    時間スケールを色々変える

--- long_and_short_memory_task_time_fixed.py
+++ long_and_short_memory_task_time_fixed.py
@@ -80,7 +80,7 @@ def main():
 
     os.makedirs("~/models", exist_ok=True)
 
-    alpha = [0.08]*50+[0.4]*450
+    alpha = [0.08]*40+[0.4]*460
     model = RecurrentNetTimeFixed(n_in=200, n_hid=args.network_size, n_out=1,
                                   use_cuda=use_cuda, alpha_weight=alpha).to(device)
     print(model)
@@ -95,7 +95,7 @@ def main():
             # time_stamp = datetime.strftime(datetime.now(pytz.timezone('Japan')), '%m%d%H%M')
             torch.save(
                 model.state_dict(),
-                "/root/models/which_pattern_008_50_04_450_epoch_{}.pth"
+                "/root/models/which_pattern_008_40_04_460_epoch_{}.pth"
                 .format(epoch))
 
 
