75b83d85b1e8103c4cced0f7d2589f57f3633d1e
tripdancer0916 <tripdancer0916@gmail.com>
2019-04-30 19:59:08 +0900

    debug

--- simple_task.py
+++ simple_task.py
@@ -96,7 +96,7 @@ def main():
             time_stamp = datetime.strftime(datetime.now(pytz.timezone('Japan')), '%m%d%H%M')
             torch.save(
                 model.state_dict(),
-                "./trained_models/{}_simple_task_all_04_epoch_{}.pth"
+                "./trained_models/{}_simple_task_all_04_epoch_{}_id_{}.pth"
                 .format(time_stamp, epoch, args.model_id))
 
 
